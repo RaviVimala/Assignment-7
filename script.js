b6aeb07fe8416755e51a62c731637f9d8cbb31df
RaviVimala <saravanaravi95@gmail.com>
2025-06-25 15:23:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+function add()
+{
+    var x=document.getElementById("N1").value;
+    var y=document.getElementById("N2").value;
+    var z=parseInt(x)+parseInt(y);
+    document.getElementById("result").innerHTML="Your Result is : "+z;
+}
+function sub(){
+    var x=document.getElementById("N1").value; 
+    var y=document.getElementById("N2").value;
+    var z=parseInt(x)-parseInt(y);
+    document.getElementById("result").innerHTML="Your Result is : "+z;
+}
+function mult()
+{
+    var x=document.getElementById("N1").value;
+    var y=document.getElementById("N2").value;
+    var z=parseInt(x)*parseInt(y);
+    document.getElementById("result").innerHTML="Your Result is : "+z;
+}
+function divi(){
+    var x=document.getElementById("N1").value;
+    var y=document.getElementById("N2").value;
+    var z=parseInt(x)/parseInt(y);
+    document.getElementById("result").innerHTML="Your Result is : "+z;
+}
+function Xsquare(){
+    var x=document.getElementById("N1").value;
+    var y=document.getElementById("N2").value;
+    var z=parseInt(x)*parseInt(x);
+    document.getElementById("result").innerHTML="Your Result is : " +z;
+}
+function Xsquare(){
+    var x=document.getElementById("N1").value;
+    var y=document.getElementById("N2").value;
+    var z=parseInt(x)*parseInt(x);
+    document.getElementById("result").innerHTML="Your Result is : " +z;
+}
+function Xcube(){
+    var x=document.getElementById("N1").value;
+    var y=document.getElementById("N2").value;
+    var z=parseInt(x)*parseInt(x)*parseInt(x);
+    document.getElementById("result").innerHTML="Your Result is : " +z;
+}
+function del(){
+    clearInterval
+}
